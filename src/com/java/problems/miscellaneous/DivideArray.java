a97e4037eef62f1769941cabc5f9ca65cbd2b0ee
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2020-10-15 00:24:06 +0530

    Modiefied Problem : Divide Array and KNearest Neighbours

--- src/com/java/problems/miscellaneous/DivideArray.java
+++ src/com/java/problems/miscellaneous/DivideArray.java
@@ -3,6 +3,10 @@
  */
 package com.java.problems.miscellaneous;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author PRATAP
  *
@@ -14,52 +18,89 @@ public class DivideArray {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String input = "False False False False False False False False";
-		pos = divide(input, 1, 8);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the string with space separated 1 and 0 : ");
+		String input = br.readLine();
+		System.out.println("Enter the value of i and j");
+		String[] str = br.readLine().split("\\s+");
+		int i = Integer.parseInt(str[0]);
+		int j = Integer.parseInt(str[1]);
+		pos = divide(input, i, j);
 		System.out.println("Initial Partition Position = " + pos);
-		pos = update(input, 1, "True", pos);
+		System.out.println("Enter the new position where update has to happen and the updated value : ");
+		str = br.readLine().split("\\s+");
+		int newPosition = Integer.parseInt(str[0]);
+		String updatedValue = str[1];
+		pos = update(input, i, newPosition, updatedValue, pos, j);
 		System.out.println("Updated Partition Position = " + pos);
 	}
 
-	private static int update(String input, int i, String b, int k) {
+	/*
+	 * By looking at the pattern for dividing the array, it can be seen that the
+	 * location at which it partitions, the number of True values on the LHS = No of
+	 * False Values on the RHS Now, if we increase a True value on the LHS, the no
+	 * of False value on the RHS also has to be incremented. So we return position
+	 * obtained from dividing the array minus 1
+	 * 
+	 * If the no of False value is incremented on the LHS, the no of True values has
+	 * to be incremented on the RHS or in other words, we have to decrement the no
+	 * of False values on the RHS
+	 * 
+	 * So, for doing this we can just increment or decrement the position by 1,
+	 * depending on the update vale Hence it will take o(1) ie. constant time.
+	 * 
+	 * Time Complexity = O(1) Space Complexity = O(1)
+	 */
+	private static int update(String input, int i, int newPosition, String b, int k, int j) {
 		// TODO Auto-generated method stub
-		String arr[] = input.split(" ");
-		if (arr[i - 1].equalsIgnoreCase(b))
-			return pos;
-		else {
-			if (b.equalsIgnoreCase("TRUE"))
+		if (newPosition >= i && newPosition <= j) {
+			if (b.equalsIgnoreCase("1"))
 				return pos - 1;
 			else
 				return pos + 1;
 		}
+		return k;
 	}
 
+	/*
+	 * Divides the array from range i to j into 2 halves having partition at 'k'
+	 * such that No of Trues in i to j = No of False in k+1 to j
+	 * 
+	 * Time Complexity = O(n) Space Complexity = O(1)
+	 */
 	private static int divide(String input, int i, int j) {
 		// TODO Auto-generated method stub
 		String[] arr = input.split(" ");
 		int left = i - 1, right = j - 1;
 		int noOfTrue = 0;
-		for (int k = left; k <= right; k++) {
-			if (arr[k].equalsIgnoreCase("TRUE"))
+		for (int k = left; k <= right; k++) { // This loop is used to calculate the total no of True values in the given
+												// range. The Time Complexity for the loop = O(n)
+			if (arr[k].equalsIgnoreCase("1"))
 				noOfTrue++;
 		}
-		if (noOfTrue == j - i + 1)
+		if (noOfTrue == j - i + 1) // if the no of True values equals the number of elements between i and j, then
+									// we return (i-1)
 			return i - 1;
 		else {
-			int noOfFalse = (j - i + 1) - noOfTrue;
-			for (int k = left; k <= right; k++) {
-				if (arr[k].equalsIgnoreCase("TRUE"))
+			int noOfFalse = (j - i + 1) - noOfTrue; // Get the no of False values in the given range by subtracting the
+													// no of True values obtained earlier
+			for (int k = left; k <= right; k++) { // Time complexity for the loop = O(n)
+				if (arr[k].equalsIgnoreCase("1")) // Used for keeping track of the current no of True Values
 					currentTrue++;
 				else
-					currentFalse++;
-				if (currentTrue == noOfFalse - currentFalse)
+					currentFalse++; // Used for keeping track of the current no of False Values
+				if (currentTrue == noOfFalse - currentFalse) // if at the current position standing, divides the array(i
+																// to j) in equal no of True Values from i to k and
+																// False values from (k+1) to j, then consider this
+																// position and break the loop
 					return k + 1;
 			}
 		}
-		return -1;
+		return -1; // if it cannot be satisfied, then we return -1
 	}
 
 }
--- src/com/java/problems/miscellaneous/KNearestNeighbours.java
+++ src/com/java/problems/miscellaneous/KNearestNeighbours.java
@@ -17,43 +17,42 @@ public class KNearestNeighbours {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 1, 2, 3, 9, 12, 13, 21, 26, 33, 43, 70, 73, 84, 100 };
-		int X = 21;
+		int X = 3;
 		int k = 4;
-		int[] kNeighbours = kNearestNeighbours(arr, X, k);
-		StringBuilder sb = new StringBuilder();
-		for (int i : kNeighbours)
-			sb.append(i + " ");
-		System.out.println(sb);
+		kNearestNeighbours(arr, X, k);
 	}
 
-	private static int[] kNearestNeighbours(int[] arr, int x, int k) {
+	private static void kNearestNeighbours(int[] arr, int x, int k) {
 		// TODO Auto-generated method stub
-		int[] output = new int[k];
+		if (k > arr.length) {
+			System.out.println("Enter proper value for k");
+			return;
+		}
 		int left, right;
-		int pos = Arrays.binarySearch(arr, x);
+		int pos = Arrays.binarySearch(arr, x); // to find the location of the element to be searched
+		StringBuilder sb = new StringBuilder();
 		if (pos < 0) {
-			pos = Math.abs(pos + 1);
-			left = pos - 1;
-			right = pos;
-		} else {
-			left = pos - 1;
-			right = pos + 1;
+			pos = Math.abs(pos + 1); // if position is less than 0, then the element is not present in the array, but
+										// if it had been there what index it should have occupied can be obtained
 		}
+		left = pos - 1; // keeping a left pointer from the position of the element
+		right = pos; // keeping a right pointer to the position of the element
 		if (right == arr.length) {
-			return Arrays.copyOfRange(arr, arr.length - k, arr.length);
-		}
-		int p = 0;
-		for (int i = 1; i <= k; i++) {
-			if (left > -1 && Math.abs(x - arr[left]) < Math.abs(x - arr[right])) {
-				output[p++] = arr[left];
-				left--;
-			} else if (right < arr.length) {
-				output[p++] = arr[right];
-				right++;
+			left = pos - k - 1; // if right pointer points outside the array, then the element is not present and we can directly take the last 'k' elements of the array
+		} else {
+			for (int i = 1; i <= k; i++) {
+				if (left > -1 && (right < arr.length && Math.abs(x - arr[left]) < Math.abs(x - arr[right]))) { // comparing left and right side with absolute value to get the nearest neighbour
+					left--;
+				} else if (right < arr.length) {
+					right++;
+				} else {
+					left -= (k - i + 1); // this condition will be satisfied when the right pointer is pointing outside the array index and we need to get the nearest neighbours only from the left side
+					break;
+				}
 			}
 		}
-		Arrays.sort(output);
-		return output;
+		for (int i = left + 1; i <= right - 1; i++)
+			sb.append(arr[i] + " "); // with the left and right pointer being obtained, we can fetch the k elements one by one in order.
+		System.out.println(sb);
 	}
-
 }
