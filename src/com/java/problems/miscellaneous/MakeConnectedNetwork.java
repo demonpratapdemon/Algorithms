9340bb33d312dc9e32486b4101edf23b9763dffe
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2020-12-11 21:34:49 +0530

    Updating the MakeConnectedNetwork problem. Used Union Find Data Structure concept to implement this.

--- src/com/java/problems/miscellaneous/MakeConnectedNetwork.java
+++ src/com/java/problems/miscellaneous/MakeConnectedNetwork.java
@@ -6,7 +6,6 @@ package com.java.problems.miscellaneous;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
  * @author PRATAP
@@ -42,55 +41,11 @@ public class MakeConnectedNetwork {
 			connections[i][0] = Integer.parseInt(input[0]);
 			connections[i][1] = Integer.parseInt(input[1]);
 		}
-		//int steps = makeConnected(n, connections);
-		int steps = makeEffectiveConnected(n, connections);
+		int steps = makeConnected(n, connections);
 		System.out.println(steps);
 	}
 
 	private static int makeConnected(int n, int[][] connections) {
-		// TODO Auto-generated method stub
-		// create a graph with adjacency list representation of the connections
-		ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
-		int totalEdges = 0;
-		for (int i = 0; i < n; i++) {
-			graph.add(new ArrayList<Node>());
-		}
-		for (int i = 0; i < connections.length; i++) {
-			int u = connections[i][0];
-			int v = connections[i][1];
-			graph.get(u).add(new Node(u, v));
-			graph.get(v).add(new Node(v, u));
-			totalEdges++;
-		}
-
-		// need to find the number of components by applying DFS
-		int components = 0;
-		boolean[] visited = new boolean[n];
-		for (int i = 0; i < n; i++) {
-			if (!visited[i]) {
-				components++;
-				dfs(graph, visited, i);
-			}
-		}
-//		System.out.println("No of components : " + components);
-
-		int redundantEdges = totalEdges - ((n - 1) - (components - 1));
-		if (redundantEdges >= components - 1)
-			return components - 1;
-		return -1;
-	}
-
-	private static void dfs(ArrayList<ArrayList<Node>> graph, boolean[] visited, int source) {
-		// TODO Auto-generated method stub
-		visited[source] = true;
-		ArrayList<Node> edgeList = graph.get(source);
-		for (Node edge : edgeList) {
-			if (!visited[edge.j])
-				dfs(graph, visited, edge.j);
-		}
-	}
-	
-	private static int makeEffectiveConnected(int n, int[][] connections) {
 		// TODO Auto-generated method stub
 		int[] parent = new int[n];
 		for (int i = 0; i < n; i++)
@@ -101,7 +56,7 @@ public class MakeConnectedNetwork {
 		for (int i = 0; i < connections.length; i++) {
 			pu = find(connections[i][0], parent);
 			pv = find(connections[i][1], parent);
-			if (pu != pv) {
+			if (pu != pv && components > 1) {
 				union(pu, pv, parent);
 				components--;
 			} else
